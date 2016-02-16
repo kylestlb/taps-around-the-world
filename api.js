e2bba04150939d11103993f952316a91fa60b997
stlegek <kyle.st.leger.barter@autodesk.com>
2016-02-15 22:32:29 -0800

    moved route to own folder

--- api.js
+++ api.js
@@ -2,18 +2,7 @@ var async = require('async');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
-var config = require(__dirname + '/config.js');
-
-var thinky = require(__dirname + '/util/thinky.js');
-var r = thinky.r;
-var Query = thinky.Query;
-var type = thinky.type;
-
-var All = require(__dirname + '/app/models/All.js');
-
 var router = express.Router();
-
 
 router.use(function(req, res, next) {
     console.log('Req sent to server at ' + Date.now());
@@ -26,39 +15,8 @@ router.get('/', function(req, res) {
     });
 });
 
-router.route('/beers')
-    .get(function(req, res) {
-        All.Beer.orderBy({
-            index: 'createdAt'
-        }).run().then(function(result) {
-            res.send(JSON.stringify(result));
-        }).error(function(error) {
-            res.send(500, {
-                error: error.message
-            });
-        });
-    })
-    .post(function(req, res) {
-        var beer = new All.Beer({
-            name: req.query.name,
-        });
-
-        beer.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({
-                message: 'beer created.'
-            });
-        });
-    });
-
-router.route('/beers/:id')
-.get(function(req, res){
-    Beer.get(id).run().then(function(beer){
-        res.json({
-            beer: beer
-        });
-    });
-});
+// Hook up each router for each model
+router.use('/beers',require('./app/routes/beers'));
+//router.use('/taplist',require('./taplist'))
 
 module.exports = router;
---
+++ app/routes/beers.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var All = require(__dirname + '/../models/All.js');
+
+var thinky = require(__dirname + '/../../util/thinky');
+var r = thinky.r;
+var Query = thinky.Query;
+var type = thinky.type;
+
+
+router.route('/')
+    .get(function(req, res) {
+        All.Beer.orderBy({
+            index: 'createdAt'
+        }).run().then(function(result) {
+            res.send(JSON.stringify(result));
+        }).error(function(error) {
+            res.send(500, {
+                error: error.message
+            });
+        });
+    })
+    .post(function(req, res) {
+        var beer = new All.Beer({
+            name: req.query.name,
+        });
+
+        beer.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({
+                message: 'beer created.'
+            });
+        });
+    });
+
+router.route('/:id')
+.get(function(req, res){
+    Beer.get(id).run().then(function(beer){
+        res.json({
+            beer: beer
+        });
+    });
+});
+
+module.exports = router;
